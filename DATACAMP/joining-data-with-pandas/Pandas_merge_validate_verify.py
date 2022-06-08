fed6babe43bda596008aecdd6a0b5ccc2baf31b9
Atif <atif.iu@gmail.com>
2022-06-08 12:45:36 +0530

    Pandas merge/join with validate argument

---
+++ DATACAMP/joining-data-with-pandas/Pandas_merge_validate_verify.py
@@ -0,0 +1,7 @@
+'''
+merge has validate arguments if set to one_to_one then it will validate if one_to_one relationship exists or not
+otherwise it will throw error
+
+Likewise Concat has verify_integrity which has default value of False but if set to True it will ensure that index
+on cancatenate dataframe has not duplicate index value
+'''
